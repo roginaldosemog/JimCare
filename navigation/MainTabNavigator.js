4bb7ebedb14bae8323a3cc1599d5359a739c9460
Igor Aragão <roginaldosemog@gmail.com>
2018-11-14 20:53:48 -0200

    Removing unused screens
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -7,9 +7,6 @@ import HomeScreen from '../screens/HomeScreen';
 import HistoryScreen from '../screens/HistoryScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
-
 const HomeStack = createStackNavigator({
   Home: HomeScreen,
 });
@@ -64,48 +61,8 @@ ProfileStack.navigationOptions = {
   ),
 };
 
-//TODO(Erase Links and Settings from Project)
-
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
-
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-link${focused ? '' : '-outline'}`
-          : 'md-link'
-      }
-    />
-  ),
-};
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={
-        Platform.OS === 'ios'
-          ? `ios-options${focused ? '' : '-outline'}`
-          : 'md-options'
-      }
-    />
-  ),
-};
-
 export default createBottomTabNavigator({
   HomeStack,
   HistoryStack,
   ProfileStack,
-  //LinksStack,
-  //SettingsStack,
 });
