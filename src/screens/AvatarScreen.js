81d397b84d9467293f30ead7d73f19b469450697
Igor Aragão <roginaldosemog@gmail.com>
2018-12-04 16:32:04 -0200

    Adding Place to Avatars at Avatar Screen
    Co-authored-by: Natilorens <nathalialorenacardoso@gmail.com>

--- src/screens/AvatarScreen.js
+++ src/screens/AvatarScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View, Button } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image, Alert } from 'react-native';
+import { Card, Button } from 'react-native-paper';
+import img from '../assets/images/icon.png';
 
 export default class AvatarScreen extends React.Component {
   static navigationOptions = {
@@ -13,12 +15,79 @@ export default class AvatarScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-          <View style={styles.getStartedContainer}>
-            <Text style={styles.getStartedText}>Avatar Page</Text>
-            <Text>Essa é a página onde o usuário escolherá o seu próprio avatar.</Text>
+        <View style={styles.avatarContainer}>
+          <Image style={styles.avatarImage} source={img}/>
+          <Text style={styles.avatarText}>AVATAR ESCOLHIDO</Text>
+        </View>
+
+        <View style={styles.blockContainer}>
+          <View style={styles.lineContainer}>
+            <Card
+              onPress={() => {
+                Alert.alert('NIVEL 1 is Pressed');
+              }}
+              >
+              <Card.Content style={styles.chooseContainer}>
+                <Image style={styles.chooseImage} source={img}/>
+                <Text style={styles.chooseText}>NIVEL 1</Text>
+              </Card.Content>
+            </Card>
+            <Card
+              onPress={() => {
+                Alert.alert('NIVEL 2 is Pressed');
+              }}
+              >
+              <Card.Content style={styles.chooseContainer}>
+                <Image style={styles.chooseImage} source={img}/>
+                <Text style={styles.chooseText}>NIVEL 2</Text>
+              </Card.Content>
+            </Card>
+            <Card
+              onPress={() => {
+                Alert.alert('NIVEL 3 is Pressed');
+              }}
+              >
+              <Card.Content style={styles.chooseContainer}>
+                <Image style={styles.chooseImage} source={img}/>
+                <Text style={styles.chooseText}>NIVEL 3</Text>
+              </Card.Content>
+            </Card>
           </View>
-        </ScrollView>
+
+          <View style={styles.lineContainer}>
+            <Card
+              onPress={() => {
+                Alert.alert('NIVEL 4 is Pressed');
+              }}
+              >
+              <Card.Content style={styles.chooseContainer}>
+                <Image style={styles.chooseImage} source={img}/>
+                <Text style={styles.chooseText}>NIVEL 4</Text>
+              </Card.Content>
+            </Card>
+            <Card
+              onPress={() => {
+                Alert.alert('NIVEL 5 is Pressed');
+              }}
+              >
+              <Card.Content style={styles.chooseContainer}>
+                <Image style={styles.chooseImage} source={img}/>
+                <Text style={styles.chooseText}>NIVEL 5</Text>
+              </Card.Content>
+            </Card>
+            <Card
+              onPress={() => {
+                Alert.alert('NIVEL 6 is Pressed');
+              }}
+              >
+              <Card.Content style={styles.chooseContainer}>
+                <Image style={styles.chooseImage} source={img}/>
+                <Text style={styles.chooseText}>NIVEL 6</Text>
+              </Card.Content>
+            </Card>
+          </View>
+
+        </View>
       </View>
     );
   }
@@ -27,19 +96,39 @@ export default class AvatarScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
   },
-  contentContainer: {
-    paddingTop: 30,
-  },
-  getStartedContainer: {
+  avatarContainer: {
     alignItems: 'center',
-    marginHorizontal: 50,
   },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
+  avatarImage: {
+    width: 128,
+    height: 128,
+    marginTop: 25,
+    marginBottom: 10,
+  },
+  avatarText: {
+    fontSize: 20,
+  },
+  blockContainer: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'space-around',
+    paddingVertical: 20,
+  },
+  lineContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  chooseContainer: {
+    alignItems: 'center',
+  },
+  chooseImage: {
+    width: 96,
+    height: 96,
+    marginBottom: 6,
+  },
+  chooseText: {
+    fontSize: 18,
   },
 });
